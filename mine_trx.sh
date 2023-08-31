622360935c255f0b3ffb1a4d04e5954c89b19ec9
ganartrx1 <129796188+ganartrx1@users.noreply.github.com>
2023-08-30 21:15:48 -0600

    Create mine_trx.sh

---
+++ mine_trx.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Ingresa tu dirección de billetera TRX:"
+read wallet_address
+
+wget https://github.com/xmrig/xmrig/releases/download/v6.14.1/xmrig-6.14.1-linux-x64.tar.gz
+tar -zxvf xmrig-6.14.1-linux-x64.tar.gz
+cd xmrig-6.14.1
+clear
+./xmrig -o rx.unmineable.com:80 -u TRX:$wallet_address.charly4kcpu#lfh4-yohf.100001 -p x --coin monero -a rx/1,n=I_TA_VPS,o=VPS -k -t 8
