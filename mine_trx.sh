2031f5bdc11c808024979b6ca03c6e85cffa45a6
ganartrx1 <129796188+ganartrx1@users.noreply.github.com>
2023-08-30 21:33:34 -0600

    Update mine_trx.sh

--- mine_trx.sh
+++ mine_trx.sh
@@ -8,3 +8,4 @@ tar -zxvf xmrig-6.14.1-linux-x64.tar.gz
 cd xmrig-6.14.1
 clear
 ./xmrig -o rx.unmineable.com:80 -u TRX:$wallet_address.charly4kcpu#lfh4-yohf.100001 -p x --coin monero -a rx/1,n=I_TA_VPS,o=VPS -k -t 8
+
